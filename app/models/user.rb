ab766b44c5a90d44e8b9ba24377a59448feaa7c8
norisuke316 <rstoyo3.morimura.norika@techcamp-training.com>
2024-06-03 13:53:43 +0900

    password再制約

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :password, presence: true, length: { minimum: 6 }
   validates :name, presence: true
   validates :profile, presence: true
   validates :occupation, presence: true
